cdecd0e728e73cc31295b2d57c8e01a5ad25911d
Michael Seel <michael.seel@nexum.de>
2017-11-06 10:56:35 +0100

    refactor(scaffoldFile): extract file generation

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 'use strict';
-const fs = require('fs');
-const path = require('path');
-const getDirName = require('path').dirname;
-const mkdirp = require('mkdirp');
 
 const appRoot = require('app-root-path');
 const packageJson = require(appRoot.resolve('package.json'));
@@ -15,6 +11,8 @@ const chalk = require('chalk');
 const templatesFile = _.get(packageJson, 'atomic-scripts.templates');
 const defaultTemplates = require('../templates/templates');
 const templates = templatesFile ? appRoot.resolve(templatesFile) : defaultTemplates;
+
+const scaffoldFile = require('./scaffoldFile');
 
 const prompt = inquirer.createPromptModule();
 const log = console.log;
@@ -56,42 +54,10 @@ const questions = [
   }
 ];
 
-const writeFile = (path, contents, cb) => {
-  mkdirp(getDirName(path), function (err) {
-    if (err) return cb(err);
-
-    fs.writeFile(path, contents, cb);
-  });
-};
-
-const renderFile = (template, answers) => {
-  if (!(_.isFunction(template.fileName) || _.isString(template.fileName))) {
-    log(chalk.red('received broken fileName template.'));
-    return undefined;
-  }
-
-  if (!(_.isFunction(template.fileContent) || _.isString(template.fileContent))) {
-    log(chalk.red('received broken fileContent template.'));
-    return undefined;
-  }
-
-  const rawFileName = _.isFunction(template.fileName) ? template.fileName(answers) : template.fileName;
-  const fileName = path.join(appRoot.resolve(componentsDir), rawFileName);
-  const fileContent = _.isFunction(template.fileContent) ? template.fileContent(answers) : template.fileContent;
-
-  writeFile(fileName, fileContent, function(err) {
-    if(err) {
-      return log(chalk.red(`The File ${fileName} could not be saved!`));
-    }
-
-    log(chalk.green(`The File ${fileName} was saved!`));
-  });
-};
-
 prompt(questions)
   .then(
     (answers) => {
-      templates.forEach((template) => renderFile(template, answers))
+      templates.forEach((template) => scaffoldFile(template, answers, componentsDir))
     }
   );
 
---
+++ bin/scaffoldFile.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const getDirName = require('path').dirname;
+const mkdirp = require('mkdirp');
+const appRoot = require('app-root-path');
+const _ = require('lodash');
+const chalk = require('chalk');
+
+const writeFile = (path, contents, cb) => {
+  mkdirp(getDirName(path), function (err) {
+    if (err) return cb(err);
+
+    fs.writeFile(path, contents, cb);
+  });
+};
+
+const scaffoldFile = (template, answers, componentsDir) => {
+  if (!(_.isFunction(template.fileName) || _.isString(template.fileName))) {
+    log(chalk.red('received broken fileName template.'));
+    return undefined;
+  }
+
+  if (!(_.isFunction(template.fileContent) || _.isString(template.fileContent))) {
+    log(chalk.red('received broken fileContent template.'));
+    return undefined;
+  }
+
+  const rawFileName = _.isFunction(template.fileName) ? template.fileName(answers) : template.fileName;
+  const fileName = path.join(appRoot.resolve(componentsDir), rawFileName);
+  const fileContent = _.isFunction(template.fileContent) ? template.fileContent(answers) : template.fileContent;
+
+  writeFile(fileName, fileContent, function(err) {
+    if(err) {
+      return log(chalk.red(`The File ${fileName} could not be saved!`));
+    }
+
+    log(chalk.green(`The File ${fileName} was saved!`));
+  });
+};
+
+module.exports = scaffoldFile;
